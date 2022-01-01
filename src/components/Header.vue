253051037b59a6e5e159cf1c099020997051d351
Deki97 <dejan97@libero.it>
2022-01-01 20:26:21 +0100

    Header done

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/img/boing-logo.png" alt="Boing Logo">
+        </div>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+header {
+    background-color: $primary-color;
+    padding: 10px 30px;
+
+    .logo {
+        width: 80px;
+    }
+}
+</style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'SingleCard'
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
