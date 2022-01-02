0060183f0511f81ed0292fcbe75163ec3a0fafb9
Deki97 <dejan97@libero.it>
2022-01-02 10:43:06 +0100

    Main statico done

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,34 @@
 <template>
     <main>
-        
+        <div class="container">
+            <div class="cards-list">
+                <SingleCard />
+            </div>
+        </div>
     </main>
 </template>
 
 
 <script>
+import SingleCard from './SingleCard.vue'
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    components: {
+        SingleCard
+    }
 }
 </script>
 
 
 <style lang="scss" scoped>
-
+main {
+    padding: 50px 0;
+    .container {
+        .cards-list {
+            display: flex;
+            flex-wrap: wrap;
+        }
+    }
+}
 </style>
--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="card">
+        <div class="card-image">
+            <img src="https://nick.mtvnimages.com/uri/mgid:arc:content:nick.com:9cd2df6e-63c7-43da-8bde-8d77af9169c7?quality=0.7" alt="Spongebob Image">
+        </div>
 
+        <div class="card-title">
+            Spongebob Squarepants
+        </div>
+
+        <div class="card-year">
+            Year: 1999
+        </div>
+
+        <div class="card-episodes">
+            Episodes: 272
+        </div>
+
+        <div class="card-creator">
+            Creator: Stephen Hillenburg
+        </div>
     </div>
 </template>
 
@@ -13,5 +31,36 @@ export default {
 
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
 
+.card {
+    width: calc((100% / 5) - 20px);
+    margin: 15px 10px;
+    
+
+    &-image {
+        img {
+            height: 250px;
+            width: 100%;
+            object-fit: cover;
+            cursor: pointer;
+        }
+    }
+
+    &-title {
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 14px;
+        color: $secondary-color;
+        cursor: pointer;
+    }
+
+    &-year,
+    &-episodes,
+    &-creator {
+        color: $primary-color;
+        font-size: 14px;
+        font-weight: bold;
+    }
+}
 </style>
