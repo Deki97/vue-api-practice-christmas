5999868b62d861460c7e30003c95a94369f978f5
Deki97 <dejan97@libero.it>
2022-01-02 12:31:59 +0100

    Cartoons API done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,10 @@
         <div class="logo">
             <img src="../assets/img/boing-logo.png" alt="Boing Logo">
         </div>
+
+        <div class="title">
+            <h1>Cartoons API</h1>
+        </div>
     </header>
 </template>
 
@@ -18,11 +22,18 @@ export default {
 @import '../style/variables.scss';
 
 header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     background-color: $primary-color;
     padding: 10px 30px;
 
     .logo {
         width: 80px;
+    }
+
+    .title {
+        color: $secondary-color;
     }
 }
 </style>
